33def6f7f719e429de18d526b3cf227bbba5bca8
Beatriz Medeiros Faria <medeirosfbia@gmail.com>
2025-02-27 21:28:07 -0300

    coments_embed_and_vector

--- embed.py
+++ embed.py
@@ -5,39 +5,38 @@ from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from get_vector_db import get_vector_db
 
+# Obtém a pasta temporária da variável de ambiente
 TEMP_FOLDER = os.getenv('TEMP_FOLDER')
 
+# Função para verificar se o arquivo é permitido (somente PDFs)
 def allowed_file(filename):
-    has_dot = '.' in filename
-    is_pdf = filename.rsplit('.', 1)[1].lower() in {'pdf'}
-
+    has_dot = '.' in filename  # Verifica se há um ponto no nome do arquivo
+    is_pdf = filename.rsplit('.', 1)[1].lower() in {'pdf'}  # Verifica se a extensão é PDF
     return has_dot and is_pdf
 
-
+# Função para salvar o arquivo temporariamente
 def save_file(file):
     ct = datetime.now()
-    ts = ct.timestamp()
-    new_filename = str(ts) + "_" + secure_filename(file.filename)
-    file_path = os.path.join(TEMP_FOLDER, new_filename)
-    file.save(file_path)
-
+    ts = ct.timestamp()  # Obtém o timestamp atual
+    new_filename = str(ts) + "_" + secure_filename(file.filename)  # Gera um nome único para o arquivo
+    file_path = os.path.join(TEMP_FOLDER, new_filename)  # Define o caminho para salvar o arquivo
+    file.save(file_path)  # Salva o arquivo no diretório temporário
     return file_path
 
+# Função para carregar e dividir os dados do arquivo PDF
 def load_and_split_data(file_path):
-    loader = UnstructuredPDFLoader(file_path=file_path)
-    data = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
-    chunks = text_splitter.split_documents(data)
-
+    loader = UnstructuredPDFLoader(file_path=file_path)  # Carrega o arquivo PDF
+    data = loader.load()  # Extrai o conteúdo do PDF
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)  # Divide o texto em partes menores
+    chunks = text_splitter.split_documents(data)  # Realiza a divisão do documento
     return chunks
 
-
+# Função para processar e armazenar os embeddings do arquivo
 def embed(file):
-    if file.filename != '' and file and allowed_file(file.filename):
-        file_path = save_file(file)
-        chunks = load_and_split_data(file_path)
-        db = get_vector_db()
-        db.add_documents(chunks)
-        os.remove(file_path)
-
+    if file.filename != '' and file and allowed_file(file.filename):  # Verifica se o arquivo é válido
+        file_path = save_file(file)  # Salva o arquivo temporariamente
+        chunks = load_and_split_data(file_path)  # Carrega e divide o arquivo
+        db = get_vector_db()  # Obtém a instância do banco de vetores
+        db.add_documents(chunks)  # Adiciona os documentos ao banco de dados vetorial
+        os.remove(file_path)  # Remove o arquivo temporário após o processamento
     return True
--- get_vector_db.py
+++ get_vector_db.py
@@ -1,13 +1,24 @@
+# Importando as bibliotecas necessárias para o script
 import os
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 
+# Variáveis de ambiente que serão utilizadas pelo código
 CHROMA_PATH = os.getenv('CHROMA_PATH')
 COLLECTION_NAME = os.getenv('COLLECTION_NAME')
 TEXT_EMBEDDING_MODEL = os.getenv('TEXT_EMBEDDING_MODEL')
 
-def get_vector_db():
-    embbeding = OllamaEmbeddings(model=TEXT_EMBEDDING_MODEL,show_progress=True)
+def criar_banco_de_dados():
+    """
+    Cria um banco de dados de vetores usando o modelo de gerador de embeddings especificado.
+    Primeiro, configura a geração das embeddings com o modelo e a barra de progresso.
+    Em seguida, cria uma instância do Chroma que permite armazenar e recuperar informações usando vetores,
+    especificando o nome da coleção e o diretório de persistência.
+    A função retorna este banco de dados de vetores como resultado final.
+    """
+
+    # Configura a geração das embeddings utilizando o modelo fornecido
+    embbeding = OllamaEmbeddings(model=TEXT_EMBEDDING_MODEL, show_progress=True)
 
     db = Chroma(
         collection_name=COLLECTION_NAME,
